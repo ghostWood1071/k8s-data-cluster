a13cc15cca61a0b941d635dcee4cbd97dadf263c
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2025-10-16 03:19:27 +0700

    update

--- k8s-data-services/k8s/orchestration/airflow/config/hostname_callable.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def get_hostname():
-    """
-    Return a static hostname for Airflow scheduler API,
-    fetched from environment variable AIRFLOW_SCHEDULER_SERVICE_NAME.
-    """
-    return os.getenv("AIRFLOW_SCHEDULER_SERVICE_NAME", "airflow-scheduler-service")
