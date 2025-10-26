8abf51a4019313e23974bdc06d29cce0bb95a493
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2025-10-26 23:36:46 +0700

    update

--- k8s-data-services/k8s/orchestration/postgres/00-create-airflow.sql
+++ k8s-data-services/k8s/orchestration/postgres/00-create-airflow.sql
@@ -5,28 +5,3 @@ BEGIN
       CREATE ROLE airflow LOGIN PASSWORD 'airflow';
    END IF;
 END$$;
-
--- Tạo database airflow, set owner = airflow
-DO $$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'airflow') THEN
-      CREATE DATABASE airflow OWNER airflow;
-   END IF;
-END$$;
-
--- Trao quyền tổng quát trên DB (owner đã đủ, nhưng grant thêm cho rõ ràng)
-GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
-
--- Chuyển ngữ cảnh sang DB airflow để set quyền schema/tương lai
-\connect airflow
-
--- Đảm bảo schema public thuộc về airflow
-ALTER SCHEMA public OWNER TO airflow;
-
--- Đặt search_path mặc định
-ALTER ROLE airflow SET search_path = public;
-
--- Mặc định quyền cho các object tạo mới trong tương lai
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES    TO airflow;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO airflow;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO airflow;
