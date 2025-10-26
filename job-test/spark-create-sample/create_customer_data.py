a46b440e83c4528e044f3dc6717d4fad3ff8be1d
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2025-10-26 15:22:11 +0700

    update

---
+++ job-test/spark-create-sample/create_customer_data.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp
+
+spark = (
+    SparkSession.builder
+    .appName("CreateDeltaTables")
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    .enableHiveSupport()
+    .getOrCreate()
+)
+
+data_customer = [
+    (1, "Alice", "alice1@gmail.com", ),
+    (2, "Bob", "bob.lol@gmail.com"),
+    (3, "Charlie", "charlice_hi@gmail.com")
+]
+
+df_customer = spark.createDataFrame(data_customer, ["id", "name", "email"]).withColumn("created_at", current_timestamp())
+
+df_customer.write.format("delta").mode("overwrite").saveAsTable("bronze.customer_raw")
+
+print("✅ Created Delta tables and registered in Hive.")
+spark.stop()
