178f54f37866f653aa5cd8f5e53b8703435a4c32
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2025-10-26 17:15:42 +0700

    update

--- job-test/spark-create-sample/create_customer_data.py
+++ job-test/spark-create-sample/create_customer_data.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import current_timestamp
+from pyspark.sql.functions import current_timestamp, date_format
 
 spark = (
     SparkSession.builder
@@ -10,6 +10,8 @@ spark = (
     .getOrCreate()
 )
 
+spark.sql("CREATE DATABASE IF NOT EXISTS bronze")
+
 data_customer = [
     (1, "Alice", "alice1@gmail.com", ),
     (2, "Bob", "bob.lol@gmail.com"),
@@ -17,8 +19,13 @@ data_customer = [
 ]
 
 df_customer = spark.createDataFrame(data_customer, ["id", "name", "email"]).withColumn("created_at", current_timestamp())
+df_customer = df_customer.withColumn("partition_date", date_format(df_customer["created_at"], "yyyy-MM-dd"))
 
-df_customer.write.format("delta").mode("overwrite").saveAsTable("bronze.customer_raw")
+(df_customer.write
+   .format("delta")
+   .mode("append")
+   .partitionBy("partition_date")
+   .saveAsTable("bronze.customer_raw"))
 
 print("✅ Created Delta tables and registered in Hive.")
 spark.stop()
